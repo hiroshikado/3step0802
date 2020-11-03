c2b0157016c283095056d6cd440601ce7546de6e
hiroshikado <okonomiyakunarahiroimono@yahoo.co.jp>
2020-11-03 20:33:55 +0900

    shopping-cart_trying

---
+++ cart.php
@@ -0,0 +1,39 @@
+<!-- ここから -->
+<!-- ここまで -->
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8" >
+	<title>ショッピングカートを作ろう</title>
+	<link rel="stylesheet" href="/php/css/skyblue.css">
+</head>
+<body>
+	<div class="bg-success padding-y-20">
+		<div class="container text-center">
+			<h1>カート一覧</h1>
+		</div>
+	</div>
+	<div class="container padding-y-20">
+		<!-- ここから -->
+		<!-- ここまで -->
+		<h2>カートの一覧</h2>
+		<!-- ここから -->
+		<!-- ここまで -->
+		<table class="table">
+			<thead>
+				<tr>
+					<th>商品名</th>
+					<th>値段</th>
+				</tr>
+			</thead>
+			<tbody>
+				<!-- ここから -->
+				<!-- ここまで -->
+			</tbody>
+		</table>
+		<!-- ここから -->
+		<!-- ここまで -->
+		<a href="shop.php" class="btn">商品一覧に戻る</a>
+	</div>
+</body>
+</html>
